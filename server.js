ff1591e6531c80714dc4024a56bf10d0914969a8
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-09 17:13:49 +0100

    not giving up on this code ;)

--- server.js
+++ server.js
@@ -109,9 +109,7 @@ if (process.env.NODE_ENV === 'production'|| process.env.NODE_ENV === 'staging') 
   };
 
 
-//app.get('*', (req, res) => {
-   // res.sendFile(path.join(__dirname, '../build'))
-//})
+
 
 
 
