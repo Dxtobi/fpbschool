c53c6148eb76358b7709a039827b180eeb96a868
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-09 15:09:06 +0100

    not giving up on this code ;)

--- server.js
+++ server.js
@@ -96,9 +96,17 @@ app.use(passport.initialize());
 //passport strategy
 require('./config/passport')(passport);
 //middleware
+//if (process.env.NODE_ENV === 'production') {
+//    app.use(express.static(path.join(__dirname, 'client/build')))
+//}
+
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-}
+    app.use(express.static('client/build')); // serve the static react app
+    app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+      res.sendFile(path.join(__dirname, './client/build/index.html'));
+    });
+    console.log('Serving React App...');
+  };
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/upload' , express.static(path.join(__dirname , '/upload')));
