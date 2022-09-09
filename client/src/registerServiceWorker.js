eb9d792f68dcbfe63b1ac3ce2ddbe8e3a4665988
Dxtobi <44758414+Dxtobi@users.noreply.github.com>
2022-09-09 14:59:18 +0100

    not giving up on this code ;)

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -8,8 +8,7 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
-const isLocalhost = false
-  /*Boolean(
+const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
@@ -17,7 +16,7 @@ const isLocalhost = false
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
-);*/
+);
 
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
